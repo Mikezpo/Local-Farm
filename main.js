b240283ef47666769430e5bffdccd6de86bfcdc0
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-03-10 00:51:05 +0000

    Updated HTML, CSS and JS code

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 // coded by @Mikezpo
 
 const project_name = 'Your local Farm';
+
+const toggleButton = document.getElementsByClassName('navButton')[0];
+const navbarLinks = document.getElementsByClassName('navLinks')[0];
+
+// Add click event listener to each menu item
+const menuItems = document.querySelectorAll('.menu-list a');
+for (const item of menuItems) {
+  item.addEventListener('click', () => {
+    // Close the menu when an item is clicked
+    navbarLinks.classList.remove('active');
+    toggle.checked = false;
+  });
+}
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
