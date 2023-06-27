de85ca391bb41ead6148abe557302b964243039e
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-06-27 17:29:14 +0100

    Updated sign in form

--- main.js
+++ main.js
@@ -40,3 +40,15 @@ function showModal(modalId) {
 function hideModal(modalId) {
   document.getElementById(modalId).style.display = 'none';
 }
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
